44aa924f2b21920b78b6433588a938e46268be46
Maykel Lopez <maykellopez@maykels-macbook-pro-1.home>
2023-11-08 18:20:19 +0100

    Add Cask Formula

---
+++ relax.rb
@@ -0,0 +1,10 @@
+cask "relax" do
+  version "0.0.1"
+  sha256 "1a36bc4c1f5be46f6ada8a788137c7195728fe79bb9525f783232d3c35cbe3fe"
+
+  url "https://github.com/z3phyro/relax/releases/download/v0.0.1/relax"
+  name "Relax"
+  homepage "https://github.com/z3phyro/relax"
+
+  binary "relax"
+end
